e06b5710b96a2f1a17573faf8938aca6a54b4c85
RUSSEN BISSEMO <russenbissemo@gmail.com>
2023-05-11 11:39:23 +0200

    toutofheroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from 'src/models/hero.model';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,17 +12,21 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService , private messageService: MessageService) {}
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-  //methode pour récupérer les héros du service
-  getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
-  }
   // Angular appelle ngOnInit() à un moment approprié après la construction de HeroesComponent instance
   ngOnInit(): void {
     this.getHeroes();
   }
+// afficher un historique de chaque fois que l'utilisateur clique sur un héros
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
+  }
+  //methode pour récupérer les héros du service
+  getHeroes(): void {
+     this.heroService.getHeroes()
+          .subscribe(heroes => this.heroes = heroes);
+  }
+  
 }
